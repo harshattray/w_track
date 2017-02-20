9571788b733f38991996c626aac3b1ba5d85e9f3
Harsha Attray <harsh.attray@gmail.com>
2017-02-20 13:00:09 +0530

    feat: setState initialised and term capture enabled from searchbar

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,12 +1,24 @@
 import React , { Component } from 'react';
 
 export default class SearchBar extends Component{
+    constructor(props){
+        super(props);
+        this.state={term :'' };
+        this.onInputChange = this.onInputChange.bind(this); // This is done to make this accessible| else use fat arrow function
+    }
+    onInputChange(event){
+        this.setState({term:event.target.value});
+        // console.log(event.target.value);
+    }
+
     render(){
         return(
+            <form>
             <div className="ui fluid action input">
-  <input type="text" placeholder="Search..."/>
+  <input type="text" placeholder="5 day forecast in any city " value={this.state.term} onChange={this.onInputChange}/>
   <div className="ui button">Search</div>
           </div>
+          </form>
         );
     }
 }

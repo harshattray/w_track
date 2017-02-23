47a8d6e571fad63d12d72565e664dc779c76f543
Harsha Attray <harsh.attray@gmail.com>
2017-02-23 16:38:35 +0530

    feat: helper function to calculate Avg values using lodash

--- src/components/spark-chart.js
+++ src/components/spark-chart.js
@@ -1,13 +1,19 @@
+import _ from 'lodash';
 import  React  from 'react';
-import  { Sparklines,SparklinesLine} from 'react-sparklines';
+import  { Sparklines,SparklinesLine,SparklinesReferenceLine} from 'react-sparklines';
 
+function avg(data){
+   return  _.round(_.sum(data)/data.length);
+}
 
 export default (props)=>{
     return(
         <div>
           <Sparklines height={100} width={150} data={props.data}>
               <SparklinesLine color={props.color}/>
+              <SparklinesReferenceLine type="avg" />
         </Sparklines>
+        <div> Average value: {avg(props.data)}</div>
         </div>
     );
 }
